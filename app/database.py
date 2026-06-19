d3d240964b3890d8ff66d9121a5bb6b363fd047a
Albert Tamaev <a87366914@gmail.com>
2026-06-19 14:20:33 +0300

    fix database url

--- app/database.py
+++ app/database.py
@@ -2,18 +2,10 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "tasks.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"sqlite+aiosqlite:///{DB_PATH}"
-
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(DATABASE_URL)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 Base = declarative_base()
-
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
