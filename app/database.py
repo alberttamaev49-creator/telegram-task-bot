62a2a1b565734d7c33820ce84392378edb5b5403
Albert Tamaev <a87366914@gmail.com>
2026-06-19 14:29:59 +0300

    fix database url

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
-import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set!")
 
 engine = create_async_engine(DATABASE_URL)
 
